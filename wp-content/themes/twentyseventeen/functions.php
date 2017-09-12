8339d6451722fb64dde85916cce81c5e9ccff785
Michele Rose <ldnallen@gmail.com>
2017-09-11 20:08:29 -0700

    Added functions and js to child theme, removed it from parent theme

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -566,58 +566,5 @@ require get_parent_theme_file_path( '/inc/customizer.php' );
  */
 require get_parent_theme_file_path( '/inc/icon-functions.php' );
 
-/* Infinity Scroll */
-
-function be_post_summary(){
-	the_title( '<h2 class="entry-title">', '</h2>' );
-}
-
-/**
- * AJAX Load More
- * @link http://www.billerickson.net/infinite-scroll-in-wordpress
- */
-function be_ajax_load_more() {
-	check_ajax_referer( 'be-load-more-nonce', 'nonce' );
-
-	$args = isset( $_POST['query'] ) ? array_map( 'esc_attr', $_POST['query'] ) : array();
-	$args['post_type'] = isset( $args['post_type'] ) ? esc_attr( $args['post_type'] ) : 'post';
-	$args['paged'] = esc_attr( $_POST['page'] );
-	$args['post_status'] = 'publish';
-
-	ob_start();
-	$loop = new WP_Query( $args );
-	if( $loop->have_posts() ): 
-		while( $loop->have_posts() ): $loop->the_post();
-			be_post_summary();
-		endwhile; 
-	endif; 
-	wp_reset_postdata();
-  
-  	$data = ob_get_clean();
-	wp_send_json_success( $data );
-	wp_die();
-}
-add_action( 'wp_ajax_be_ajax_load_more', 'be_ajax_load_more' );
-add_action( 'wp_ajax_nopriv_be_ajax_load_more', 'be_ajax_load_more' );
-
-
-/**
- * Javascript for Load More
- *
- */
-function be_load_more_js() {
-	global $wp_query;
-	$args = array(
-		'url'   => admin_url( 'admin-ajax.php' ),
-		'query' => $wp_query->query,
-	);
-			
-	wp_enqueue_script( 'be-load-more', get_stylesheet_directory_uri() . '/assets/js/loadmore.js', array( 'jquery' ), '1.0', true );
-	wp_localize_script( 'be-load-more', 'beloadmore', $args );
-	
-}
-add_action( 'wp_enqueue_scripts', 'be_load_more_js' );
-
-
 
 
